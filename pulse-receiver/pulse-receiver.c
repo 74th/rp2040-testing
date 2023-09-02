cfcec700d3294c08c216d248c21ad0191f4df37b
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2023-09-02 21:06:31 +0900

    receiver

--- pulse-receiver/pulse-receiver.c
+++ pulse-receiver/pulse-receiver.c
@@ -7,12 +7,84 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define IR_IN_PIN 8
+#define BUFFER_SIZE 1024
+
 int main()
 {
     stdio_init_all();
+
+    printf("initialize\r\n");
+
+    gpio_init(IR_IN_PIN);
+    gpio_set_dir(IR_IN_PIN, GPIO_IN);
+
+    gpio_pull_up(IR_IN_PIN);
+
     while (true)
     {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        run();
     }
+}
+
+int run()
+{
+
+    u_int32_t buffer[BUFFER_SIZE];
+
+    memset(buffer, 0, sizeof(buffer));
+
+    printf("waiting next start...\r\n");
+
+    u_int32_t prev = time_us_32();
+    while (gpio_get(IR_IN_PIN) == 1)
+    {
+        u_int32_t now = time_us_32();
+        if (now - prev > 1000000)
+        {
+            bool v = gpio_get(IR_IN_PIN);
+            printf(".\n", v);
+            prev = now;
+        }
+    }
+    buffer[0] = time_us_32();
+    bool running = true;
+
+    for (int i = 1; i < BUFFER_SIZE && running; i++)
+    {
+        while (gpio_get(IR_IN_PIN) == 0)
+            ;
+
+        prev = time_us_32();
+        buffer[i] = prev;
+        i++;
+
+        while (gpio_get(IR_IN_PIN) == 1)
+        {
+            if (time_us_32() - prev > 1000000)
+            {
+                printf("done\n");
+                running = false;
+                break;
+            }
+        }
+
+        buffer[i] = time_us_32();
+    }
+
+    printf("pulse = [");
+    for (int i = 1; i < BUFFER_SIZE; i++)
+    {
+        if (buffer[i] == 0)
+        {
+            break;
+        }
+        if (i > 1)
+        {
+            printf(",");
+        }
+        printf("%d", buffer[i] - buffer[i - 1]);
+    }
+    printf("]\r\n");
+    sleep_ms(1000);
 }
--- pulse-receiver/tasks.py
+++ pulse-receiver/tasks.py
@@ -8,7 +8,7 @@ elf_file = pathlib.Path("build", "pulse-receiver.elf")
 def upload(c):
     if c.run("picotool info", warn=True).failed:
         c.run("reset-bootsel /dev/ttyACM0", warn=True)
-        time.sleep(3)
+        time.sleep(1)
     c.run(f"picotool load {elf_file}")
     c.run(f"picotool reboot")
 
@@ -17,3 +17,8 @@ def build(c):
     with c.cd("build"):
         c.run("cmake ..")
         c.run("make")
+
+@task
+def monitor(c):
+    time.sleep(1.5)
+    c.run("cat /dev/ttyACM0")
